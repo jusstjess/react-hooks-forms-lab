1f0ba2fdfbe1c0afa2f78e19b9e7624729150d50
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-11 12:46:13 -0700

    completed lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,28 +1,41 @@
 import React, { useState } from "react";
 import ItemForm from "./ItemForm";
-import Filter from "./Filter";
 import Item from "./Item";
+import Filter from "./Filter";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items,addNewItem }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const[searchText, setSearchText] = useState('')
+  
+  function handleSearchText(e){
+    setSearchText(e.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = items
+    .filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
-  });
+  })
+    .filter((item)=>{
+      return item.name.toLowerCase().includes(searchText.toLowerCase())
+    })
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+        onItemFormSubmit={addNewItem} />
+      <Filter 
+        search={searchText}
+        onSearchChange={handleSearchText}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id}  category={item.category} name={item.name} />
         ))}
       </ul>
     </div>
